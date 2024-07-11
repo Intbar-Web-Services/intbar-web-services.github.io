70a64fb0bc4e4fccbdd18d1dc44b44d489e31c17
Tiramisu <165537498+tiramisyuz@users.noreply.github.com>
2024-07-11 11:33:09 -0500

    rack

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt="girls kissing" src="https://media1.tenor.com/m/Wgk_0iJebWUAAAAC/anime-girl-kiss-akademi-online.gif"></img>
+      <p>probe</p>
+    </div>
+  );
+}
+
+export default App;
